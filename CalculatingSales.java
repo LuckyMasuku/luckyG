c01f9be6c6fa42700532e10de363cbe3331c52c4
LuckyMasuku <36404741+LuckyMasuku@users.noreply.github.com>
2018-02-26 12:03:20 +0000

    Add files via upload
    Week 21

---
+++ CalculatingSales.java
@@ -0,0 +1,41 @@
+// Lucky Masuku
+import java.util.Scanner;
+public class CalculatingSales{
+	public static void main(String [] args){
+		double total = 0;
+		int productId;
+
+		System.out.print("Please enter a product number: ");
+		pruductId = input.nextInt();
+
+		while(product ! = 0){
+			system.out.print("Enter qauntity sold: ");
+			int quantity = input.nextInt();
+
+			switch(productId){
+				case 1:
+				total += qauntity * 2.98;
+				break;
+		    	case 2:
+							total += qauntity * 4.50;
+				break;
+					case 3:
+								total += qauntity * 9.98;
+				break;
+					case 4:
+								total += qauntity * 4.49;
+				break;
+					case 5:
+								total += qauntity * 6.87;
+				break;
+				}
+			}else if(productId ! = 0)
+			System.out.println("Product must be 1 to 5");
+			System.out.println("Please enter number product number (0 to stop): ");
+		}
+		System.out.print("Total of product sold is: ");
+		System.out.println("Math.round(total*10000.0/10000.0))
+
+}
+}
+
