fbe810e51733e5a9f1389823094a075888aa1db3
LuckyMasuku <36404741+LuckyMasuku@users.noreply.github.com>
2018-02-27 13:54:44 +0000

    Add files via upload

---
+++ Methods.java
@@ -0,0 +1,22 @@
+// Lucky Masuku
+
+    public class Methods{
+	public static void main(String [] args){
+		Method m = new Methods();
+		m.method();
+		Index.main(null);
+		public void method1(){
+			System.out.println("Method 1 here and I am calling method 2");
+
+
+		}method2();
+		public void method2(){
+			System.out.println("Hie method 1, from method 2, I am calling method 3");
+
+		}method3();
+
+		public void method3(){
+					System.out.println("Hie method 2, from method 3, tell method 1 I say hello.");
+
+	}
+}
