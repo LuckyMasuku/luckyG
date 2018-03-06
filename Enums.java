b3c986770c51f8d7f2472112f3a28f7bdd4e4fad
LuckyMasuku <36404741+LuckyMasuku@users.noreply.github.com>
2018-03-06 18:48:34 +0000

    Add files via upload

--- Enums.java
+++ Enums.java
@@ -1,26 +1,26 @@
 //Lucky masuku
+
 public class Enums{
 	public enum Month{
-		January, February, March, April, May, June,July, August, September, October, November,december
-	}
-	public enum Day{
-		Sunday, Monday, Tuesday, Wednesday, Thursday, Friday, Saturday
-	}
-	public  static void main(String[]args){
-		int months = Month.values().length;
-		int days = Day.values().length;
-		System.out.println("Number of months in a year: "+months);
-		System.out.println("Number of days in a week: "+days+"\n");
+		JANUARY, FEBRUARY,MARCH,APRIL,MAY,JUNE,JULY,AUGUST,SEPTEMBER,OCTOBER,NOVEMBER,DECEMBER
+		{
+			public enum Day{
+				SUNDAY, MONDAY, TUESDAY, WEDNESDAY, THURSDAY, FRIDAY, SATURDAY
+			}
+			public static void main(String[]args){
+				int months = Month.values().length;
+				int days = days.values().length;
+				System.out.println("Number of months in a year:"+months);
+				System.out.println("Number of days in a week: "+days+"\n");
 
-		System.out.println("Months:\n");
-		for(int i = 0; i< months; i++){
-			System.out.println(Month.values()[i]);
+				System.out.println("Months: \n");
+				for(int i = 0; i < months; i++){
+					System.out.println(Month.values()[i]);
+				}
+				System.out.println("\nDays: \n");
+				for(Days d: Day.values()){
+					System.out.println(d);
+				}
+				System.out.println();
 		}
-		System.out.println("\nDays: \n");
-		for(Day d: Day.values()){
-			System.out.println(d);
-		}
-		System.out.println();
 	}
-}
-
