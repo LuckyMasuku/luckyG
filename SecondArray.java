79214e3ba65587e254f1fa7b8e955211319f6848
LuckyMasuku <36404741+LuckyMasuku@users.noreply.github.com>
2018-03-13 13:56:18 +0000

    Add files via upload

---
+++ SecondArray.java
@@ -0,0 +1,15 @@
+//Lucky masuku
+
+public  class FirstArray{
+public static void main(String[]args){
+
+	int [] x = {128,132,8156,18}
+	x = new int[4];
+	System.out.println("Index\tValue\n");
+
+	for(int i = 0 ;< x.length; i++){
+		System.out.println(i+"\t"+x[i]);
+}
+Index.main(null);
+}
+}
---
+++ ThirdArray.java
@@ -0,0 +1,18 @@
+
+public  class ThirdArray{
+public static void main(String[]args){
+
+
+	x = new int[5];
+
+	System.out.println("Index\tValue\n");
+
+	for(int i = 0 ;i < x.length; i++){
+	x[i] = + 1;
+	}
+	for(int i = 0 ;i < x.length; i++){
+		System.out.println(i+"\t"+x[i]);
+}
+Index.main();
+}
+}
